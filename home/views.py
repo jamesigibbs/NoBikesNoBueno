f689938cf75c85e3cc3de34564e7ea6b0b79ad83
jamesigibbs <jamesigibbs180@gmail.com>
2021-05-04 15:04:44 +0000

    Product data displaying on home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+from products.models import Product
 # Create your views here.
 
 
 def index(request):
     """ A view to render index.html """
+    
+    products = Product.objects.all
 
-    return render(request, 'home/index.html')
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home/index.html', context)
