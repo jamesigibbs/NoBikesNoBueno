343a8e0fda7a4880010337003780da00f421b9fa
jamesigibbs <jamesigibbs180@gmail.com>
2021-05-13 10:40:12 +0000

    Created product view in home view

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from home.models import Product
 # Create your views here.
 
@@ -12,3 +12,15 @@ def index(request):
     }
 
     return render(request, 'home/index.html', context)
+
+
+def product(request, product_id):
+    """ A view to show more details on the each product """
+    products = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'home/product.html', context)
+
