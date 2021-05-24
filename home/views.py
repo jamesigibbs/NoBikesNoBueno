370d1e7a36967835dc5911eaf051b812d7cbcb59
jamesigibbs <jamesigibbs180@gmail.com>
2021-05-24 22:51:26 +0000

    Added sorting fuctionailty

--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from home.models import Product
+
+from django.db.models.functions import Lower
 # Create your views here.
 
 
@@ -9,8 +11,22 @@ def home(request):
     """ A view to render index.html and products in to cards"""
     products = Product.objects.all()
     query = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -19,9 +35,12 @@ def home(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
+        'current_sorting': current_sorting
     }
 
     return render(request, 'home/index.html', context)
