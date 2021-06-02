1ef1f87112261b495f5d05c1dcc3f2b683a169b5
jamesigibbs <jamesigibbs180@gmail.com>
2021-06-02 13:06:44 +0000

    added html to shopping cart ready to edit

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from home.models import Product
 
 
 def bag_contexts(request):
@@ -6,8 +8,19 @@ def bag_contexts(request):
     bag_items = []
     total = 0
     product_count = 0
-    delivery = 10 #Want to make this configable in the admin
+    delivery = 10 # Want to make this configable in the admin
+    bag = request.session.get('bag', {})
     
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     grand_total = delivery + total
 
     context = {
--- bag/views.py
+++ bag/views.py
@@ -13,7 +13,7 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    bag = request.POST.get('bag', {})
+    bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
@@ -21,5 +21,5 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
+
     return redirect(redirect_url)
