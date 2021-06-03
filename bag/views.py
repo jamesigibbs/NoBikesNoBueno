0ce79ee6fe646bf1fb0abe07e13ad282b7fc87f7
jamesigibbs <jamesigibbs180@gmail.com>
2021-06-03 13:33:38 +0000

    Created checkout app and edited models from boutique ado

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -26,10 +26,9 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    """ Add an item to the shopping bag """
+    """ Adjust an item in the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
@@ -37,7 +36,35 @@ def adjust_bag(request, item_id):
     else:
         bag[item_id] = quantity
 
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
     request.session['bag'] = bag
 
-    return redirect(redirect_url)
+    return redirect(reverse('bag_view'))
 
+
+def remove_item(request, item_id):
+    """ Add an item to the shopping bag """
+    try:
+        quantity = int(request.POST.get('quantity'))
+        bag = request.session.get('bag', {})
+
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+
+        if quantity > 0:
+            quantity = 0
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+
+        return HttpResponse(status=200)
+    except:
+        return HttpResponse(status=500)
