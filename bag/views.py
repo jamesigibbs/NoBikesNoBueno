e4413a4865d445f03acf526a50fd8fb6f1d083e6
jamesigibbs <jamesigibbs180@gmail.com>
2021-06-11 11:58:29 +0000

    Added basic stripe funstionality from boutique ado

--- bag/views.py
+++ bag/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 
 def bag_view(request):
     """ A view to render the shopping bag page """
-    print(request)
 
     return render(request, 'bag/bag.html')
 
