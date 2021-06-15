c3e508df1442fa83af9b299b059cb7b0c1ed5869
jamesigibbs <jamesigibbs180@gmail.com>
2021-06-15 13:25:06 +0000

    Added views templates and modals to profile app

--- NoBikesNoBueno/urls.py
+++ NoBikesNoBueno/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('contact/', include('contact.urls')),
     path('riders/', include('riders.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
