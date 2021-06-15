68a629b895659bb447ae9796ce2adb68cf49d876
jamesigibbs <jamesigibbs180@gmail.com>
2021-06-15 16:44:23 +0000

    Made log in functional and tested profile

--- NoBikesNoBueno/settings.py
+++ NoBikesNoBueno/settings.py
@@ -175,7 +175,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #Email Settings
 DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
