04a9b59b425ed0087d75b1e939fb6dfe0e620506
jamesigibbs <jamesigibbs180@gmail.com>
2021-06-15 23:43:17 +0000

    Email fully functional

--- NoBikesNoBueno/settings.py
+++ NoBikesNoBueno/settings.py
@@ -106,8 +106,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -175,7 +173,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #Email Settings
 DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
