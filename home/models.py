6395542e8b392361e8e6c232d693c535661dc2c7
jamesigibbs <jamesigibbs180@gmail.com>
2021-06-21 23:18:24 +0000

    Edit rider bios, change quantity buttons

---
+++ home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    sku = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- riders/models.py
+++ riders/models.py
@@ -4,8 +4,7 @@ from django.db import models
 
 class Rider(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    description = models.TextField(max_length=250, null=False, blank=False)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    description = models.TextField(max_length=500, null=False, blank=False)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
