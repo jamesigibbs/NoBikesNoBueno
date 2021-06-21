f4f04b3836518c21e16afe5bcffe206b00df2a63
jamesigibbs <jamesigibbs180@gmail.com>
2021-06-21 20:37:50 +0000

    Changed buttons on all pages

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
+from checkout.models import Order
 from .models import UserProfile
 from .forms import UserProfileForm
 
