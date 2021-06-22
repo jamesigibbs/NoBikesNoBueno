64fbc869ff35d02717bcc53449862059495a0884
jamesigibbs <jamesigibbs180@gmail.com>
2021-06-22 02:01:46 +0000

    Fixed Database typo

--- NoBikesNoBueno/settings.py
+++ NoBikesNoBueno/settings.py
@@ -125,7 +125,7 @@ WSGI_APPLICATION = 'NoBikesNoBueno.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASEs = {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
