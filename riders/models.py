f9f9d8f5187e288234a5974e3cf134ec1d527265
jamesigibbs <jamesigibbs180@gmail.com>
2021-06-22 17:30:20 +0000

    Reset Databases and loaded new data

--- riders/models.py
+++ riders/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Rider(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(max_length=500, null=False, blank=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
